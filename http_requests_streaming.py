c31834e3d0f2745a6719a5c88a6056d82cd6dff8
Phillip Kim <philkim78@gmail.com>
2016-07-27 18:19:00 +1000

    Completed first Spark Streaming framework

--- http_requests_streaming.py
+++ http_requests_streaming.py
@@ -1,19 +1,28 @@
 import sys
 from pyspark import SparkContext
-from pyspark import SparkConf
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print >> sys.stderr, "Usage: http_requests_streaming <some string>"
+    if len(sys.argv) < 3:
+        print >> sys.stderr, "Usage: http_requests_streaming <broker> <topic>"
         exit(-1)
-        
+    
+    broker = sys.argv[1]
+    topic = sys.argv[2]  
+    
     sc = SparkContext()
+
+    # Configure the Streaming Context with a 1 second batch duration
+    ssc = StreamingContext(sc,5)
+
+    # Creat DStream using KafkaUtils
+    kafka_stream = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": broker})
     
-    
-    input_string = sys.argv[1]
-    
-    print "The input is:", input_string 
-    
-    
+    # Test
+    kafka_stream.pprint(1)
+
     # End of the application
+    ssc.start()
+    ssc.awaitTermination()
     sc.stop()
